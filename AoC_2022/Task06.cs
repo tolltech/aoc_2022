9b1fce14640409640b4877b7d33832e2241bc845
tolltech <alexandrovpe@gmail.com>
2022-12-06 10:43:39 +0400

    06

---
+++ AoC_2022/Task06.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2022
+{
+    [TestFixture]
+    public class Task06
+    {
+        [Test]
+        [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+        [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+        [TestCase(@"Task06.txt", 1896)]
+        public void Task(string input, int expected)
+        {
+            input = (File.Exists(input) ? File.ReadAllText(input) : input).Trim();
+
+            var result = 0;
+            var substring = new LinkedList<char>();
+            for (var i = 0; i < input.Length; ++i)
+            {
+                substring.AddLast(input[i]);
+
+                if (substring.Count < 4) continue;
+
+                if (substring.Distinct().Count() == 4)
+                {
+                    result = i + 1;
+                    break;
+                }
+
+                substring.RemoveFirst();
+            }
+
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ AoC_2022/Task06_2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2022
+{
+    [TestFixture]
+    public class Task06_2
+    {
+        [Test]
+        [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+        [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+        [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+        [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+        [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+        [TestCase(@"Task06.txt", 3452)]
+        public void Task(string input, int expected)
+        {
+            input = (File.Exists(input) ? File.ReadAllText(input) : input).Trim();
+
+            var result = 0;
+            var substring = new LinkedList<char>();
+            for (var i = 0; i < input.Length; ++i)
+            {
+                substring.AddLast(input[i]);
+
+                if (substring.Count < 14) continue;
+
+                if (substring.Distinct().Count() == 14)
+                {
+                    result = i + 1;
+                    break;
+                }
+
+                substring.RemoveFirst();
+            }
+
+            result.Should().Be(expected);
+        }
+    }
+}
